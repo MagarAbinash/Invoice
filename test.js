5784a11229955d841d59f75c8d08f208075b4ccc
MagarAbinash <58334187+MagarAbinash@users.noreply.github.com>
2020-06-10 08:16:12 +0545

    another update

--- test.js
+++ test.js
@@ -2,10 +2,11 @@ document.getElementById('itemAddForm').addEventListener('submit', addItem);
 
 var items = [];
 
-var itemId = 5;
+var itemId = 0;
+var sub_total = [];
+// var total = document.getElementById('subTotal');
 
 function addItem(event) {
-    // console.log("Adding item now");
     var itemName = document.getElementById('itemAddName').value;
     var itemPrice = document.getElementById('itemAddPrice').value;
     var itemQuantity = document.getElementById('itemAddQuantity').value;
@@ -36,79 +37,55 @@ function addItem(event) {
     };
     items.push(newItem);
 
-    fetchItems();
+    showCart();
+
+    subTotal(newItem.price, newItem.qty);
 
     event.preventDefault();
 }
-
-function fetchItems() {
-    var itemList = document.getElementById('items');
-    itemList.innerHTML = "";
-
-    for (var i = 0; i < items.length; i++) {
-        // console.log(items[i].name+ " Price: "+ items[i].price);
-        itemList.innerHTML += '<div class="card card-flex float-left m-3" style="width: 15rem;">' +
-            '<h1 class="card-img-top">' + items[i].name + '</h1>' +
-            '<div class="card-body">' +
-            '<h5 class="card-title">Price: ' + items[i].price + ' </h5>' +
-            '<h5 class ="card-title">Quantity: '+ items[i].qty + '</h5>'+
-            '<a href="#" onclick="addTo(\'' + items[i].id + '\')" class="btn btn-danger">Add to cart</a>' +
-            '</div></div>';
-    };
-};
-
-fetchItems();
-
-var cart = [];
-// var bills = [];
-
-function addTo(list) {
-    cart.push(list);
-    // copyToBills();
-    showCart();
-}
-
-// function copyToBills() {
-//     for (let i = 0; i < cart.length; i++) {
-//         for (let j = 0; j < items.length; j++) {
-//             if (items[j].id == cart[i]) {
-//                 bills.push({
-//                     id: items[j].id,
-//                     name: items[j].name,
-//                     qty: items[j].qty,
-//                     price: items[j].price
-//                 });
-//             }
-//         }
-//     }
-//     console.log(bills);
-// }
 
 
 function showCart() {
     var cartEle = document.getElementById('cart');
     cartEle.innerHTML = "";
-    for (let index = 0; index < cart.length; index++) {
-        for (let j = 0; j < items.length; j++) {
-            if (items[j].id == cart[index]) {
-                cartEle.innerHTML += '<tr>' +
-                    '<th scope="row">' + (index + 1) + '</th>' +
-                    '<td>' + items[j].name + '</td>' +
-                    // '<td class="w-25">' + 
-                    //     '<div class="md-form">' +
-                    //         '<input id="q" onclick="calculateAmt()" type="number" min="0" value="0" id="numberExample" class="form-control">'+
-                    //     '</div>'+ 
-                    // '</td>' +
-                    '<td>' + items[j].qty + '</td>' +
-                    '<td>' + items[j].price + '</td>' +
-                    '<td>Amount</td>' +
-                    '<td><button class="btn btn-primary btn-sm btn-inline" type="submit">Edit</button><button class="btn btn-danger btn-sm btn-inline" type="submit">Delete</button></td></tr>';
-            }
+    for (let index = 0; index < items.length; index++) {
+        cartEle.innerHTML += '<tr>' +
+            '<th scope="row">' + items[index].id + '</th>' +
+            '<td>' + items[index].name + '</td>' +
+            '<td>' + items[index].qty + '</td>' +
+            '<td>' + items[index].price + '</td>' +
+            '<td>'+ calculateAmt(items[index].price, items[index].qty) +'</td>' +
+            '<td><button class="btn btn-primary btn-sm btn-inline" onclick="editInvoice(\'' + items[index].id + '\')">Edit</button>'+
+            '<button class="btn btn-danger btn-sm btn-inline" onclick ="deleteInvoice(\''+ items[index].id +'\')">Delete</button></td></tr>';
+    }
+}
+
+function editInvoice(id) {
+    console.log("Edit me "+ id);
+}
+
+function deleteInvoice(id) {
+    for (let i = 0; i < items.length; i++) {
+        if (items[i].id == id) {
+            console.log("Doing splice");
+            items.splice(i, 1);
+            showCart();
         }
     }
 }
 
-function calculateAmt(event) {
-    var qt = document.getElementById('q');
-    console.log(qt.value);
+function calculateAmt(price, qty) {
+    var amt =  price * qty;
+    return amt;
 }
+
+function subTotal(price, qty) {
+    sub_total.push(price * qty);
+    // showTotal();
+}
+
+// function showTotal() {
+//     var t = sub_total.reduce((a, b) => a+b, 0);
+//     console.log(t)
+//     total.innerHTML = "<div>This shit why not working</div>";
+// }
